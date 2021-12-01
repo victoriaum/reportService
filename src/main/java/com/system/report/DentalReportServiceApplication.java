f03b9980ccbdb0fb068b137f551b564c72449380
victoriaum <y.ilpyo95@gmail.com>
2021-12-01 21:30:25 +0900

    Initial commit

---
+++ src/main/java/com/system/report/DentalReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.system.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalReportServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DentalReportServiceApplication.class, args);
+  }
+
+}
